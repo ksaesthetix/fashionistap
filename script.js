c6966551e2e0ed3445a293060eff77111af859fe
fashionistap <fashionistap.info@gmail.com>
2026-09-16 14:22:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// fashionistap main page HELP CENTER button
+function scrollToBottom(){
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+    });
+}
+// fashionistap main page  Our Story button
+function goToOurStory(){
+    window.location.href = 'ourstory.html';
+}
+// fashionistap main & Our Story page  Web Dev button
+function goToWebDev(){
+    window.location.href = 'webdev.html';
+}
+// Our Story page  Home button
+function goToHome(){
+    window.location.href = 'index.html';
+}
+// Our Story page  Help Center button
+function goToHelpCenter(){
+    window.location.href = 'index.html#Email';
+}
+// Web Dev Page 
